5096a505c3ebfca8dfaa2d9a1c3adcf2bf5abe63
Micah Mones <a00901580@se14l0120-17.ad.bcit.ca>
2019-03-20 11:31:58 -0700

    created base for main.js

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//******************************************
+//PRIMARY ELEMENTS
+//******************************************
+
+
+
+//******************************************
+//OBJECTS AND HOLDERS
+//******************************************
+
+//array holding objects and info for all exercises
+//could split up if they are too many exercises if you want
+//e.g: push, pull, full body, etc
+var exercises = [ bench_press ];
+
+//just an example
+var bench_press = {
+  name: 'Bench Press',
+  desc: '',
+  img: 'bench.jpg'
+};
+
+//******************************************
+//UI STATES
+//******************************************
+
+
+
+//******************************************
+//FUNCTIONS TO CHANGE STATES
+//******************************************
+
+
+
+//******************************************
+//FUNCTIONS TO CHANGE UI
+//******************************************
+
+
+
