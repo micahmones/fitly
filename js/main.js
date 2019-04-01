b8bc3c68e7796cc3f22efefda171ec63569394e4
unknown <micahmones@gmail.com>
2019-03-31 17:32:42 -0700

    reworked homepage and added logo

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 //array holding objects and info for all exercises
 //could split up if they are too many exercises if you want
 //e.g: push, pull, full body, etc
-var exercises = [ bench_press ];
+var exercises = [bench_press];
 
 //just an example
 var bench_press = {
@@ -37,8 +37,17 @@ var bench_press = {
 //******************************************
 
 //testing this
-function goBack(){
+function goBack() {
   window.history.back();
 }
 
-
+//fading in listener on homepage
+document.addEventListener('DOMContentLoaded', setTimeout(
+  function () {
+    setTimeout(function () {
+      document.getElementsByClassName('body-content')[0].classList.remove('fade');
+    }, 150); // delay for fitly logo
+    setTimeout(function () {
+      document.getElementsByClassName('blue-btn')[0].classList.remove('fade')
+    }, 2000); // delay for get started button
+  }, 100)); // overall delay upon page load
