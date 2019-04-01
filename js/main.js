d18fece803846dafe61ef3450b26c3856c5ac84b
Micah Mones <a00901580@se14l0124-16.ad.bcit.ca>
2019-04-01 12:13:21 -0700

    working on hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 //******************************************
 //PRIMARY ELEMENTS
 //******************************************
+var menu = document.querySelector(".menu"),
+    toggle = document.querySelector(".menu-toggle");
 
+function toggleToggle() {
+  toggle.classList.toggle("menu-open");
+  toggleMenu()
+};
+
+function toggleMenu() {
+  menu.classList.toggle("active");
+};
+
+//toggle.addEventListener("click", toggleToggle, false);
+//toggle.addEventListener("click", toggleMenu, false);
 
 
 //******************************************
