8fe7326d3096463a18712d85f4c23c5abe441300
unknown <micahmones@gmail.com>
2019-04-22 17:58:32 -0700

    index page changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //fade for logo and button
 function timeOut() {
   setTimeout(function () {
-    document.getElementsByClassName('body-content')[0].classList.remove('fade');
+    document.getElementsByClassName('landing-logo')[0].classList.remove('fade');
   }, 150); // delay for fitly logo
   setTimeout(function () {
     document.getElementsByClassName('blue-btn')[0].classList.remove('fade')
